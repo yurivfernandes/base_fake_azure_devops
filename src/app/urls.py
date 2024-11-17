5f3d6d4d87b55073153d926a009ba7ab57e84df6
Yuri Viana Fernandes <yuri.viana.fernandes@gmail.com>
2024-11-17 17:55:36 -0300

    2024-11-17 Criado os arquivos necessários para início do projeto pbi.
    - Atualizado o arquivo requirements.txt com as dependências necessárias.
    - Criado o arquivo src/1_create_base_consultores.py para criar a base de dados de consultores.
    - Criado o arquivo src/2_create_base_product_backlog.py para criar a base de dados do product backlog.
    - Removido os arquivos desnecessários do projeto Django.
    - Criado o arquivo src/bases_excel/faltas_consultores.xlsx para armazenar as faltas dos consultores.

--- src/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/base-fake/", include("base_fake.urls")),
-]
